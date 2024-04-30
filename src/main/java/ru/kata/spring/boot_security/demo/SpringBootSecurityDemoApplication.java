3c93fa741e3e20aa2efb9f52ea008553664f2358
akynbekov <kindji1580>
2024-04-30 14:27:50 +0300

    bootstrap

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -9,11 +9,7 @@ import java.util.logging.Logger;
 public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
-		Logger logger = Logger.getLogger(SpringBootSecurityDemoApplication.class.getName());
-
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-
-		logger.info("Приложение запустилось. Перейдите по адресу http://localhost:8080/");
 	}
 
 }
